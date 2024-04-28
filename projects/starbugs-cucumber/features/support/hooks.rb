9afc99b932e362de905d114bff8a36c83278d18a
luceliapaiva <paivaluce@gmail.com>
2024-04-28 17:32:20 -0300

    first commit

---
+++ projects/starbugs-cucumber/features/support/hooks.rb
@@ -0,0 +1,24 @@
+
+require_relative './pages/home.rb'
+require_relative './pages/checkout.rb'
+require_relative './pages/order.rb'
+require_relative './pages/components.rb'
+
+Before do
+    @home = HomePage.new
+    @checkout = CheckoutPage.new
+    @order = OrderPage.new
+    @popup = Popup.new
+end
+
+After do |scenario|
+
+    screenshot = page.save_screenshot("logs/screenshots/#{scenario.__id__}.png")
+    attach(screenshot, "image/png", "Screenshot") 
+
+    #para ter screenshot da tela de erro caso o cenario falhe
+    #if (scenario.failed?) 
+        #screenshot = page.save_screenshot("logs/screenshots/temp.png")
+        #attach(screenshot, "image/png", "Screenshot")
+    #end
+end
